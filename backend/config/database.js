cab2df1d866d14e2b5d5c58b809d3cd183fbf549
Abhishek <73600362+abhiishekrana@users.noreply.github.com>
2024-06-09 12:55:15 +0530

    Update database.js
    For the deployemnet

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const MONGO_URI="mongodb+srv://anjanabhishek7017:twitterclone@cluster0.enox4gv.mongodb.net/"
+// const MONGO_URI="mongodb+srv://anjanabhishek7017:twitterclone@cluster0.enox4gv.mongodb.net/"
+const MONGO_URI="mongodb+srv://anjanabhishek7017:<password>@cluster0.enox4gv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const databaseConnection = () =>{
     mongoose.connect(MONGO_URI).then(()=>{
         console.log("Connected to moongoose")
